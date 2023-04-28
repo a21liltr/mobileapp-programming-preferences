4a70da9e982f53bb32cfbd6d947a7abc4c36e7d0
a21liltr <a21liltr>
2023-04-28 08:56:02 +0200

    Added SharedPreferences and SharedPreferences.Editor

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences myPreferenceRef = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
+
+        TextView prefTextRef = findViewById(R.id.tv_output);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+
+        // TODO: Launch SecondActivity
+
+        // TODO: get data from shared preferences
+
+        // SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        // int defaultValue = getResources().getInteger(R.integer.saved_high_score_default_key);
+        // int highScore = sharedPref.getInt(getString(R.string.saved_high_score_key), defaultValue);
+
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // TODO: write data to shared preferences
+        // use SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        // write to shared preferences:
+        // SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        // SharedPreferences.Editor editor = sharedPref.edit();
+        // editor.putInt(getString(R.string.saved_high_score_key), newHighScore);
+        // editor.apply();
     }
 }
